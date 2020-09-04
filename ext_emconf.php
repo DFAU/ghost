2895ff91017c9b30cfdc157dd412654fa015a8d9
Henrik Elsner <helsner@dfau.de>
2020-09-04 16:57:35 +0200

    style: reformat emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,38 +1,38 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'DFAU Background Jobqueue',
-	'description' => '',
-	'category' => 'be',
-	'shy' => 0,
-	'version' => '0.0.0',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => 'bottom',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 0,
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'DFAU Background Jobqueue',
+    'description' => '',
+    'category' => 'be',
+    'shy' => 0,
+    'version' => '0.0.0',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => 'bottom',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'beta',
+    'uploadfolder' => 0,
     'createDirs' => 'typo3temp/GhostQueues',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
-	'author' => 'Thomas Maroschik',
-	'author_email' => 'tmaroschik@dfau.de',
-	'author_company' => 'DFAU',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '7.0.0-0.0.0',
-			'typo3' => '7.6.0-10.99.99'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-);
+    'modify_tables' => '',
+    'clearcacheonload' => 1,
+    'lockType' => '',
+    'author' => 'Thomas Maroschik',
+    'author_email' => 'tmaroschik@dfau.de',
+    'author_company' => 'DFAU',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.0.0-0.0.0',
+            'typo3' => '7.6.0-10.99.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'suggests' => [
+    ],
+];
 
