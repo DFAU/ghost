9fd551e0845ce36d4e7cda11e11346245ace846c
Henrik Elsner <helsner@dfau.de>
2020-10-14 09:32:31 +0200

    feat: update code base to full v10 support/dependency

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,13 +6,9 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ghost']['connections'][\DFAU\Ghost\CmsCo
         'className' => \Bernard\QueueFactory\PersistentFactory::class,
         'arguments' => [
             'driver' => function () {
-                if (class_exists(\TYPO3\CMS\Core\Database\ConnectionPool::class)) {
                     /** @var \TYPO3\CMS\Core\Database\ConnectionPool $connectionPool */
                     $connectionPool = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class);
                     return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bernard\Driver\Doctrine\Driver::class, $connectionPool->getConnectionForTable('bernard_messages'));
-                } else {
-                    return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\DFAU\Ghost\Driver\Typo3DbDriver::class);
-                }
             },
             'serializer' => function () {
                 return new \Bernard\Serializer();
